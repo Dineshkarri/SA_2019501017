5a69edac317f4d39e8fa416130a966c8bb2c217a
Dineshkarri <dinesh97.karri@msitprogram.net>
2020-04-19 21:21:45 +0530

    Task3

--- project1/models.py
+++ project1/models.py
@@ -7,3 +7,12 @@ class Data(db.Model):
     Username = db.Column(db.String, primary_key=True, nullable=False)
     Password = db.Column(db.String, nullable=False)
     Timestamp = db.Column(db.String, nullable=False)
+
+class Book(db.Model):
+    __tablename__ = "books"
+    id = db.Column(db.Integer, primary_key=True)
+    isbn = db.Column(db.String, nullable=False)
+    title = db.Column(db.String, nullable=False)
+    author = db.Column(db.String, nullable=False)
+    pub_year = db.Column(db.Integer, nullable=False)
+    revs = db.Column(db.String, nullable=True)
